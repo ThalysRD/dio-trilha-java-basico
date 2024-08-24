18ad86f2d3ac4f73d65ffb1eb53f2d8e553e2b04
ThalysRD <tgbr66@gmail.com>
2024-08-24 12:43:38 -0300

    Estudo de condicionais compostas

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -3,11 +3,13 @@ public class CaixaEletronico {
         
 
         double saldo = 25.0;
-        double valorSolicitado = 26;
+        double valorSolicitado = 22.0;
 
 
         if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo insuficiente");
         }
 
         System.out.println("Seu saldo é " + saldo + " Reais");
