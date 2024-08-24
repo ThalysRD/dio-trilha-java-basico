e820117eff9138a45649129467999f948f58a253
ThalysRD <tgbr66@gmail.com>
2024-08-24 16:19:57 -0300

    Estudo do uso do for em arrays de maneira abreviada

--- controle-fluxo/src/ExemploFor.java
+++ controle-fluxo/src/ExemploFor.java
@@ -2,8 +2,8 @@ public class ExemploFor {
     public static void main(String[] args) {
         String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
 
-        for (int x = 0; x < alunos.length; x++){
-            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        for (String aluno: alunos){
+            System.out.println("O nome do aluno é: " + aluno);
         }
         }
 }
