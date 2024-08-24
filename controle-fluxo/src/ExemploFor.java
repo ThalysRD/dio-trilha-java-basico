d0b3bc43bff23b5b55fc9aa638a763f825575943
ThalysRD <tgbr66@gmail.com>
2024-08-24 16:18:23 -0300

    Estudo de for em arrays

--- controle-fluxo/src/ExemploFor.java
+++ controle-fluxo/src/ExemploFor.java
@@ -1,9 +1,9 @@
 public class ExemploFor {
     public static void main(String[] args) {
-        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
-            System.out.println("Contando carneirinhos: " + carneirinhos);
-        }
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
 
-        System.out.println("Joãozinho dormiu");
-    }
+        for (int x = 0; x < alunos.length; x++){
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+        }
 }
